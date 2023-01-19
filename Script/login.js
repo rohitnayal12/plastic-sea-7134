dd54166678074b5292fce52997ec10027589e42b
rohitnayal12 <rohitnayalkld@gmail.com>
2023-01-19 20:44:31 +0530

    DONE

---
+++ Script/login.js
@@ -0,0 +1,33 @@
+let loginbtn=document.getElementById("loginbtn");
+loginbtn.addEventListener("click",validate);
+user_record=JSON.parse(localStorage.getItem("userslist"))
+console.log(user_record)
+function validate(){
+
+
+ 
+    
+
+    let emailEl=document.getElementById("email").value
+    let passwordEl=document.getElementById("password").value
+
+
+    
+    
+    user_record.forEach((element) => {
+
+        if((emailEl==element.email) && (passwordEl==element.password))
+        {
+            alert("Login pass");
+        }
+        else{
+            alert("login fail")
+        }
+        
+    });
+   
+
+
+
+
+}
---
+++ Script/signup.js
@@ -0,0 +1,35 @@
+let signupbtn=document.getElementById("signupbtn");
+signupbtn.addEventListener("click",savedata);
+let signinbtn=document.getElementById("signinbtn")
+signinbtn.addEventListener("click",redirect);
+
+
+function redirect(){
+   window.open()
+}
+
+
+function savedata(){
+
+
+ 
+ let name=document.getElementById("name").value
+ let email=document.getElementById("email").value
+ let password=document.getElementById("password").value
+
+//  console.log(name)
+ let obj={
+    name: name,
+    email: email,
+    password: password,
+ };
+ 
+ let userslist= JSON.parse(localStorage.getItem("userslist"))||[]
+
+userslist.push(obj);
+
+
+localStorage.setItem("userslist",JSON.stringify(userslist))
+alert("Signup successful")
+}
+
